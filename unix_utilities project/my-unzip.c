c4a9fb43fb993eaa2f5b801d58a14a146322c921
emmampakarinen <118518912+emmampakarinen@users.noreply.github.com>
2023-02-05 16:47:01 +0200

    Update my-unzip.c
    adding name and date

--- unix_utilities project/my-unzip.c
+++ unix_utilities project/my-unzip.c
@@ -1,3 +1,5 @@
+// Emma Pakarinen, 22.1.2023
+
 /* Object: my-unzip tool simply does the reverse of the my-zip tool, taking in a compressed file and writing 
 (to standard output again) the uncompressed results. */
 
