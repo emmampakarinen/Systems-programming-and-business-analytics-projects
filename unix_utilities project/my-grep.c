3e72b08d245e599ba5553e5861f9ff75750610cf
emmampakarinen <118518912+emmampakarinen@users.noreply.github.com>
2023-02-05 16:45:15 +0200

    Update my-grep.c
    adding name and date

--- unix_utilities project/my-grep.c
+++ unix_utilities project/my-grep.c
@@ -1,3 +1,5 @@
+// Emma Pakarinen, 15.1.2023
+
 /* object: my-grep, a variant of the UNIX tool grep. This tool looks through a file, line by line, trying to find a user-specified 
 search term in the line. If a line has the word within it, the line is printed out, otherwise it is not. */
 
